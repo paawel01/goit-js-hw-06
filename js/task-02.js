6dd0a4ac536931e3947ac815c00a83cdfbe19f2d
paawel01 <pawelzylowski.01@gmail.com>
2024-03-26 14:52:06 +0100

    task01/02 update

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngredients = document.querySelector('#ingredients');
+ingredients.forEach(ingredient => {
+  const liElement = document.createElement('li');
+  liElement.textContent = ingredient;
+  liElement.classList.add('item');
+  ulIngredients.appendChild(liElement);
+});
