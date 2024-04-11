452cf7a7dd72c7993ad830cd143714173cae20eb
paawel01 <pawelzylowski.01@gmail.com>
2024-04-11 15:08:30 +0200

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const fontSizeControl = document.getElementById('font-size-control');
+const textSpan = document.getElementById('text');
+fontSizeControl.addEventListener('input', () => {
+    const fontSize = fontSizeControl.value + 'px';
+    textSpan.style.fontSize = fontSize;
+});
